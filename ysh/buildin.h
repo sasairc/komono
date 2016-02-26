8d50bd43c696acf171b5e12168c8d0a83ad06220
sasairc <sasairc@ssiserver.moe.hm>
2016-02-26 23:05:53 +0900

    yshを分割

--- ysh/buildin.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUILDIN_H
-#define BUILDIN_H
-
-#include "./ysh.h"
-
-extern int ysh_chdir(char** args);
-extern int ysh_exit(cmd_t* cmd);
-
-#endif
--- ysh/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define PROGNAME    "ysh"
-
-#define MAXLEN      2048
-#define PROMPT      "% "
-
-#endif
