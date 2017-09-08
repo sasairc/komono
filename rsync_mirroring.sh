e02fefabc564b4faf6d5f9f01da0bb83523eed1a
sasairc <sasairc@ssiserver.moe.hm>
2017-09-08 20:59:58 +0900

    modified:   rsync_mirroring.sh

--- rsync_mirroring.sh
+++ rsync_mirroring.sh
@@ -7,6 +7,7 @@ SRC=("/usr/src" "/var" "/tmp" "/home")
 DEST=("/mnt/backup_1" "/mnt/backup_2" "/mnt/backup_3" "/mnt/backup_4" "/mnt/backup_5")
 RSYNC="/usr/bin/rsync"
 RSYNC_OPT="-av --delete --exclude=lost+found --exclude=swap.img"
+RSYNC_NICE="ionice -c 2 nice -n 16"
 LOGFILE="/var/log/rsync_backup.log"
 
 # check process
@@ -17,7 +18,7 @@ PID=$(pgrep -d ', ' -f 'rsync\s(.+)')   &&  \
 # exec rsync
 for ((i = 0; i < ${#SRC[@]}; i++)); do
     for ((j = 0; j < ${#DEST[@]}; j++)); do
-        $RSYNC $RSYNC_OPT "${SRC[$i]}/" "${DEST[$j]}${SRC[$i]}/" 2>> "${LOGFILE}"
+        ${RSYNC_NICE} ${RSYNC} ${RSYNC_OPT} "${SRC[$i]}/" "${DEST[$j]}${SRC[$i]}/" 2>> "${LOGFILE}"
         test $? -eq 0   && \
             echo "$(LANG=C date): $RSYNC $RSYNC_OPT ${SRC[$i]}/ ${DEST[$j]}${SRC[$i]}/: done." >> "${LOGFILE}"    || \
             echo "$(LANG=C date): $RSYNC $RSYNC_OPT ${SRC[$i]}/ ${DEST[$j]}${SRC[$i]}/: failure." >> "${LOGFILE}"
