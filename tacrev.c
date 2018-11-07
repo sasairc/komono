637433fb1b03ac8c8ed29160ed7854f77a371a9c
sasairc <sasairc@ssiserver.moe.hm>
2018-11-07 19:40:42 +0900

    new file:   tacrev.c

---
+++ tacrev.c
@@ -0,0 +1,73 @@
+/*
+ * tacrev.c - tac と rev でやれ
+ *
+ * gcc tacrev.c -lbenly_typestring -o tacrev
+ *
+ */
+#include <benly/typestring.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static
+int read_file(FILE* fp, size_t* lines, STRING*** dest)
+{
+    size_t      th_lines    = 256;
+
+    STRING**    s           = NULL;
+
+    if ((s = (STRING**)
+                malloc(sizeof(STRING*) * (th_lines + 1))) == NULL) {
+        perror("malloc"); goto ERR;
+    }
+    *lines = 0;
+    while (fgetline(fp, &s[*lines]) != EOF) {
+        if (string_errno < 0)
+            goto ERR;
+        if (*lines == (th_lines - 1)) {
+            th_lines += th_lines;
+            if ((s = (STRING**)
+                        realloc(s, sizeof(STRING*) * th_lines)) == NULL) {
+                perror("realloc"); goto ERR;
+            }
+        }
+        (*lines)++;
+    }
+    *dest = s;
+
+    return 0;
+
+ERR:
+    th_lines = 0;
+    if (s != NULL) {
+        while (th_lines < *lines) {
+            if (s[th_lines] != NULL) {
+                s[th_lines]->release(s[th_lines]);
+            }
+            th_lines++;
+        }
+        free(s);
+    }
+
+    return -1;
+}
+
+int main(int argc, char* argv[])
+{
+    size_t      lines   = 0;
+
+    STRING**    s       = NULL;
+
+    if (read_file(stdin, &lines, &s) < 0)
+        return 1;
+    while (lines > 0) {
+        lines--;
+        s[lines]->chomp(&s[lines]);
+        s[lines]->reverse(&s[lines]);
+        fputline(stdout, s[lines]);
+        s[lines]->release(s[lines]);
+        putchar('\n');
+    }
+    free(s);
+
+    return 0;
+}
